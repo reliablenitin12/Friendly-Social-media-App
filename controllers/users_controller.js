eae5255a2ff078fecda475eaa4a92dee1675362c
Nitin <nk345264@gmail.com>
2022-06-23 22:52:55 +0530

    Manula Authentication |user Profile Details of user conpleted

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,9 +3,22 @@ const User=require('../models/user');
 
 module.exports.profile=function(req,res)
 {
-    return res.render('user_profile',{
-        title:"Friendly | Profile"
-    });
+   if(req.cookies.user_id)
+   {
+    User.findById(req.cookies.user_id,function(err,user){
+        if(user){
+           return res.render('user_profile',{
+            title:"Friendly | Profile",
+            user:user
+          })
+        }
+        return res.redirect('users/sign-in');
+    })
+   }
+   else
+   {
+    return res.redirect('users/sign-in');
+   }
 }
 
 
