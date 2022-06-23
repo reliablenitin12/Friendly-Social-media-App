502c496c6c2b91fb9eb27070273de60ae9ce1711
Nitin <nk345264@gmail.com>
2022-06-23 17:44:50 +0530

    seted views for home and users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home=function(req,res){
-   return res.end('<h1>Friendly Home_Page</h1>');
+   return res.render('home',{
+      title:"Friendly | Home"
+   });
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 module.exports.profile=function(req,res)
 {
-    return res.end("<h1>User'profile</h1>");
+    return res.render('users',{
+        title:"Friendly | Profile"
+    });
 }
