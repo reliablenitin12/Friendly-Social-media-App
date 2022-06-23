9d5e1ce3c7869b6b79172d111010c6861aed232d
Nitin <nk345264@gmail.com>
2022-06-23 21:43:44 +0530

    Manual Authetication | Sign Up Completed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User=require('../models/user');
+
+
 module.exports.profile=function(req,res)
 {
     return res.render('user_profile',{
@@ -25,6 +28,28 @@ module.exports.signUp=function(req,res)
 //get the sign up data
  module.exports.create=function(req,res)
  {
+   if(req.body.password!=req.body.confirm_password)
+   {
+    return res.redirect('back');
+   }
+   User.findOne({email:req.body.email},function(err,user){
+      if(err)
+      {
+        return console.log("Error in finding user in signing up",err);
+      }
+      if(!user)
+      {
+        User.create(req.body, function(err,user){
+            if(err)
+            {
+                return console.log("Error in creating user while signing up",err);
+            }
+           return res.redirect('/users/sign-in');
+        });
+      }else{
+        return res.redirect('back');
+      }
+   });
 
  }
 //get the sign in data and create the session for the user
