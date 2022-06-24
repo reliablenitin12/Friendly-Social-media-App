a5501c810113a074714cc60ac8f1aae5950837d8
Nitin <nk345264@gmail.com>
2022-06-24 20:40:21 +0530

    delete post completed

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
-const Post = require('../models/post')
+const Post = require('../models/post');
+const Comment =require('../models/comment');
 
 module.exports.create = function(req, res){
     Post.create({
@@ -11,4 +12,27 @@ module.exports.create = function(req, res){
             }
         return res.redirect('back');
     });
+}
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id, function(err,post){
+        if(err)
+        {
+            return console.log("error in finding post while deleting the post",err);
+        }
+        //.id converts the object id into string
+        if(post.user==req.user.id){
+          post.remove();
+
+          Comment.deleteMany({post: req.params.id},function(err){
+            if(err)
+            {
+                return console.log("Error in finding comments while deleting",err);
+            }
+            return res.redirect('back');
+          });
+        }else{
+            return res.redirect('back');
+        }
+    })
 }
